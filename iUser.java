244324459dd7a29f1a30b922e8e15ba767ea8526
Isaac O'Brien <38996271+oizey77@users.noreply.github.com>
2022-04-20 14:23:19 +1000

    Basic User Class and Interface

---
+++ iUser.java
@@ -0,0 +1,14 @@
+package Users;
+
+public interface iUser {
+
+    //user Fields
+    String userID = new String();
+    String Name = new String();
+    String permission = new String();
+    String role = new String();
+
+
+
+
+}
