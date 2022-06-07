c65464d980bbdb8fb771e9a3635655deabe91702
DungDang25 <dungdang25@hotmail.com>
2022-06-07 16:48:45 +1000

    Merging from Marcus branch
    - merged JComponentLibrary and MazeJFrame from Marcus branch

--- src/MazeGUI/MazeJFrame.java
+++ src/MazeGUI/MazeJFrame.java
@@ -1,16 +1,15 @@
 /**
  * Author: Marcus Nguyen
- * Developed for CAB 302 project
- * QUT student 2022
  */
 package src.MazeGUI;
 
-import MazeGUI.MazeCreatorComponents.JComponentLibrary;
+import src.MazeGUI.MazeCreatorComponents.JComponentLibrary;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.EventListener;
 
 public class MazeJFrame extends JFrame {
 
@@ -18,43 +17,75 @@ public class MazeJFrame extends JFrame {
      * Variable Declaration
      */
     public JPanel MazePanel, ControllerPanel;
-    public JButton btnLeft,btnRight,btnTop,btnBottom,btnLogout, btnGenerate, btnSolution;
+    public JButton btnLeft,btnRight,btnTop,btnBottom,btnClose,btnGenerateMaze;
     public JLabel lblFocused_X,lblFocused_Y;
-    private int CellSize = 50;
+    private int X_MazeSize;
+    private int Y_MazeSize;
+    private int CellSize = 10;
+    private Maze maze;
     /**
      * Constructor
      *
-     * @param Width  The Width of the frame
-     * @param Height The Height of the frame
+     * @param X_MazeSize  The X_MazeSize of the frame
+     * @param Y_MazeSize The Y_MazeSize of the frame
      */
-    public MazeJFrame(int Width, int Height) {
-
+    public MazeJFrame(int X_MazeSize, int Y_MazeSize) {
+        this.X_MazeSize=X_MazeSize;
+        this.Y_MazeSize=Y_MazeSize;
+        this.setVisible(true);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setTitle("Maze Generator");
-        this.setBounds(50, 50, (Width+2) * CellSize+ (Width*2) + 250 , (Height+2) * CellSize+ (Height*2));
-        this.setMinimumSize(new Dimension(1000,500));
+        this.setBounds(50, 50, (X_MazeSize+2) * CellSize+ (X_MazeSize*2) + 250 , (Y_MazeSize+2) * CellSize+ (Y_MazeSize*2));
+        this.setMinimumSize(new Dimension(1000,800));
         this.getContentPane().setLayout(null);
 
-        MazePanel = JComponentLibrary.CreateJPanel(this,50,50,(Width) * CellSize+4,(Height) * CellSize+4,null,true);
+
+        MazePanel = JComponentLibrary.CreateJPanel(this,50,50,(X_MazeSize) * CellSize+4,(Y_MazeSize) * CellSize+4,null,true);
         ControllerPanel = JComponentLibrary.CreateJPanel(this,this.getWidth()-300,50,200,this.getHeight()-120,null,true);
 
-        btnTop = JComponentLibrary.CreateButton(ControllerPanel,50,0,100,50,Color.WHITE,Color.BLACK,"T",true);
-        btnLeft = JComponentLibrary.CreateButton(ControllerPanel,0,50,50,100,Color.WHITE,Color.BLACK,"L",true);
-        btnRight = JComponentLibrary.CreateButton(ControllerPanel,150,50,50,100,Color.WHITE,Color.BLACK,"R",true);
-        btnBottom = JComponentLibrary.CreateButton(ControllerPanel,50,150,100,50,Color.WHITE,Color.BLACK,"B",true);
-        btnGenerate = JComponentLibrary.CreateButton(ControllerPanel,0,ControllerPanel.getHeight()-150,200,50,Color.WHITE,Color.BLACK,"Generate Maze",true);
-        btnSolution = JComponentLibrary.CreateButton(ControllerPanel,0,ControllerPanel.getHeight()-100,200,50,Color.WHITE,Color.BLACK,"Generate Optimal Solution",true);
-        btnLogout = JComponentLibrary.CreateButton(ControllerPanel, 0, ControllerPanel.getHeight()-50,200,50,Color.WHITE, Color.BLACK,"Back",true);
-        btnLogout.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                MainGUI.closeMazeEdit();
-                MainGUI.openMazeC();
-            }
-        });
+        btnTop = JComponentLibrary.CreateButton(ControllerPanel,50,0,100,50,"T",true);
+        btnLeft = JComponentLibrary.CreateButton(ControllerPanel,0,50,50,100,"L",true);
+        btnRight = JComponentLibrary.CreateButton(ControllerPanel,150,50,50,100,"R",true);
+        btnBottom = JComponentLibrary.CreateButton(ControllerPanel,50,150,100,50,"B",true);
+        btnClose = JComponentLibrary.CreateButton(ControllerPanel, 0, ControllerPanel.getHeight()-50,200,50,"Close",true);
+        btnGenerateMaze = JComponentLibrary.CreateButton(ControllerPanel,0,300,200,50,"Generate Maze",true);
+
         lblFocused_X=JComponentLibrary.CreateJLabel(ControllerPanel,55,75,"Focused X:",95,15,Color.BLACK,true);
         lblFocused_Y=JComponentLibrary.CreateJLabel(ControllerPanel,55,105,"Focused Y:",95,15,Color.BLACK,true);
+        this.repaint();
+
+        btnClose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CloseFrame();
+            }
+        });
+
+        btnGenerateMaze.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GenerateNewMaze();
+            }
+        });
+
+        btnLeft.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                maze.GenerateSolution();
+            }
+        });
+    }
+
+    private void CloseFrame(){
+        this.dispose();
+    }
+
+    private void GenerateNewMaze() {
+        this.MazePanel.removeAll();
+        this.repaint();
+        maze = new Maze(X_MazeSize,Y_MazeSize);
+        maze.GenerateMaze(this);
         this.repaint();
     }
 }
